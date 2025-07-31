5175c8c5f4d6e59771b757f3c9dbbba96b7f93a0
ChadProbert <chadcprobert@gmail.com>
2025-07-31 19:55:37 +0200

    Improved colour switching between light/dark mode for the footer background and text.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,8 @@
+import "./Footer.css";
+
 export default function Footer() {
   return (
-    <footer className="relative z-10 flex justify-center p-4 bg-black">
+    <footer className="footer-container relative z-10 flex justify-center p-4">
       Chad Probert &copy; {new Date().getFullYear()}
     </footer>
   );
