7f95ddb2107c1856a6a99adf84ea34179abfef77
ChadProbert <chadcprobert@gmail.com>
2025-07-31 19:54:04 +0200

    Cleaning up and improving the look of the navigation button for mobile users.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,9 @@
 
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
-import Image from "next/image";
 import { X, Menu } from "lucide-react";
 import ThemeToggleButton from "./ThemeToggleButton";
+import "./Navbar.css";
 
 const navigation = [
   { name: "Home", href: "#hero" },
@@ -20,7 +20,7 @@ export default function Navbar() {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
       <nav className="flex items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
+        <div className="logo-container flex lg:flex-1">
           <a href="#hero" className="-m-1.5 p-1.5">
             <span className="sr-only">Chad Probert</span>
             <svg
@@ -51,7 +51,7 @@ export default function Navbar() {
             className="-m-2.5 inline-flex items-center justify-center p-2.5"
           >
             <span className="sr-only">Open main menu</span>
-            <Menu aria-hidden="true" className="size-6" color="black" />
+            <Menu aria-hidden="true" className="size-6" color="var(--foreground)" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -78,22 +78,13 @@ export default function Navbar() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Chad Probert</span>
-              <Image
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                width={36}
-                height={36}
-              />
-            </a>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5"
+              className="mt-1.5"
             >
               <span className="sr-only">Close menu</span>
-              <X aria-hidden="true" className="size-6" color="black" />
+              <X aria-hidden="true" className="size-6" color="black" strokeWidth={3} />
             </button>
           </div>
           <div className="mt-6 flow-root">
