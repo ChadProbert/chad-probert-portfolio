afda056d24c251e1e23d4e7d05c6777a3a8f7256
ChadProbert <chadcprobert@gmail.com>
2025-08-25 02:10:36 +0200

    No longer using server actions.

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb",
-      allowedOrigins: ["http://localhost:3000"], // ! UPDATE once deployed to include domain.
-    },
-  },
 };
 
 export default nextConfig;
