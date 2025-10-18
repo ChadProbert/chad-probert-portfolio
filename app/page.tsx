cafc083072226e50c99d3c083fa1f20fafcfb2fc
Chad Probert <chadcprobert@gmail.com>
2025-10-18 18:16:06 +0200

    Code cleanup and removing of the rocket image in the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { Skills } from "@/components/skills/Skills";
 import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
-import { HeroRocketImage } from "@/components/HeroRocketImage";
 
 export default function Home() {
   return (
@@ -13,18 +12,7 @@ export default function Home() {
       <main>
         <Navbar />
         <section className="mx-auto px-0 pt-14 lg:px-8 mb-20 lg:mb-50">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-            <div className="order-2 lg:order-1">
-              <Hero />
-            </div>
-            <div className="order-1 hidden lg:block lg:order-2">
-              <div className="relative flex items-center justify-end">
-                <div className="absolute z-10">
-                  <HeroRocketImage />
-                </div>
-              </div>
-            </div>
-          </div>
+          <Hero />
         </section>
         <About />
         <Skills />
