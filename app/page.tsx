5adeed5d91c17f284c7e24284fbac376d86d2b50
Chad Probert <chadcprobert@gmail.com>
2025-10-18 02:39:29 +0200

    Added a new footer component to house copyright information

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { About } from "@/components/About";
 import { Skills } from "@/components/skills/Skills";
 import { Projects } from "@/components/Projects";
 import { Contact } from "@/components/Contact";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -35,6 +36,7 @@ export default function Home() {
         <Skills />
         <Projects />
         <Contact />
+        <Footer />
       </main>
     </div>
   );
