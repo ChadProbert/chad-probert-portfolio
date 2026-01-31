50b009efa1064e1de44e0724c68ca00e003b7b59
ChadProbert <chadcprobert@gmail.com>
2026-01-31 15:18:46 +0200

    reduced width of skills marquee

--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -3,11 +3,13 @@ import { MarqueeBottom } from "./MarqueeBottom";
 
 export const Skills = () => {
   return (
-    <div id="skills" className="mb-20 lg:mb-50">
-      <h2 className="text-3xl font-bold sm:text-5xl var(--foreground) mb-10 sm:mb-20 text-center tracking-wide">
+    <div id="skills" className="mb-20">
+      <h2 className="mt-20 text-3xl font-medium sm:text-5xl var(--foreground) text-center tracking-wide">
         Skills
       </h2>
-      <MarqueeTop />
+      <div className="mt-10 sm:mt-20">
+        <MarqueeTop />
+      </div>
       <MarqueeBottom />
     </div>
   );
